dff21a117db895e3888ea91179c3cb5da06be1b1
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-11 09:19:13 -0500

    additional exercise from curriculum bonus material

--- js/lab-pod-exercises.js
+++ js/lab-pod-exercises.js
@@ -69,4 +69,55 @@ function golfScore(par, strokes) {
 
 golfScore(5, 4);
 
+
+// Exercise #3 from Planets Array Curriculum Exercise with BONUS
+
+        var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+        // var planetsArray;
+
+        /**
+         * TODO:
+         * Convert planetsString to an array, and save it in a variable named
+         * planetsArray.
+         * console.log planetsArray to check your work
+         */
+
+        console.log(planetsString);
+
+        var planetsArray = planetsString.split('|');
+
+        console.log(planetsArray);
+
+
+
+        /**
+         * TODO:
+         * Create a string with <br> tags between each planet. console.log() your
+         * results. Why might this be useful?
+         *
+         * BONUS:
+         * Create another string that would display your planets in an undordered
+         * list. You will need an opening AND closing <ul> tags around the entire
+         * string, and <li> tags around each planet.
+         */
+
+
+
+            // var planetsReString = planetsArray.join('<br> ');
+            //
+            // console.log(planetsReString);
+
+
+
+
+            // BONUS EXERCISE
+
+
+        var planetsList = "<ul><li>" + planetsArray.join("</li><li>") + "</li></ul>";
+
+        console.log(planetsList);
+
+
+
+
 })();
