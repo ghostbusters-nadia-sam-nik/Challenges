7ddad44e7157619fe4e5629a4db538ae02adf4d8
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-18 15:22:54 -0500

    added functionality

--- js/thankunext.js
+++ js/thankunext.js
@@ -2,33 +2,44 @@
 
 window.onload = function() {
     var box = document.getElementById("box");
+
+    var position = 0;
+
+    var questions = [
+        'Is he kind?',
+        'Is he currently in a relationship or otherwise legally/emotionally committed to someone else?',
+        'Does he have a job?',
+    ];
+
     document.getElementById("yes").onclick = function() {
+        position++;
+        document.getElementById('questions')
+        .innerHTML = questions[position];
         box.style.backgroundImage = 'url("img/yes.jpg")';
     };
+
     document.getElementById("no").onclick = function() {
         box.style.backgroundImage = 'url("img/nope.jpg")';
     };
-    document.getElementById("box").onmouseleave = function() {
-        box.style.backgroundImage = 'url("img/other.jpg")';
-    };
+
+    // document.getElementById("box").onmouseleave = function() {
+    //     box.style.backgroundImage = 'url("img/other.jpg")';
+    // };
+
+    document.getElementById('questions')
+        .innerHTML = questions[position];
 
 };
 
 
 
-var questions = [
-    'Is he kind?',
-    'Is he currently in a relationship or otherwise legally/emotionally committed to someone else?',
-    'Does he have a job?',
-];
-
-var i = 0;  // the index of the current item to show
-
-setInterval(function() {            // setInterval makes it run repeatedly
-    document.getElementById('questions')
-        .innerHTML = questions[i++];    // get the item and increment i to move to the next
-    if (i == questions.length) i = 0;   // reset to first element if you've reached the end
-}, 15000);
+// var i = 0;  // the index of the current item to show
+//
+// setInterval(function() {            // setInterval makes it run repeatedly
+//     document.getElementById('questions')
+//         .innerHTML = questions[i++];    // get the item and increment i to move to the next
+//     if (i == questions.length) i = 0;   // reset to first element if you've reached the end
+// }, 15000);
 
 
 
