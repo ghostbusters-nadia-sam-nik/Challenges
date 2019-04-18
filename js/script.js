c21660d84622b879f6a6ca1ddc7fed7e0e226399
Rosales <48695183+rosalesnikho@users.noreply.github.com>
2019-04-18 15:59:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,6 +42,23 @@ function readLine(r) {
 };
 
 
+///////// Ojbjects Exercises //////////
 
+
+Object.objsize = function(Myobj) {
+    var osize = 0, key;
+    for (key in Myobj) {
+        if (Myobj.hasOwnProperty(key)) osize++;
+    }
+    return osize;
+};
+
+var student = { 
+name : "Name of Student", 
+sclass : "VI", 
+rollno : 12 };
+
+var objsize = Object.objsize(student);
+console.log('Size of the current object : '+objsize);
 
 
